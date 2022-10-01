952328dc161c8369f41b5047a1dadc4fd1e862c8
altunog <altunog@gmail.com>
2022-10-01 09:48:24 +0300

    Project folders rearranged.

---
+++ iTunes Client App/iTunes Client App/Scenes/Main/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunes Client App
+//
+//  Created by Oğuz Kaan Altun on 1.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
